a6d58e7e8a776c68cee0b6879579740467d84550
Neto Pompeu <neto1@hotmail.fr>
2021-03-12 16:33:42 +0100

    ajout fichier JS

---
+++ function.js
@@ -0,0 +1,11 @@
+//functions
+function FaireQuelqueChose() {
+    console.log('je fais un truc');
+}
+const FaireUneTache = (type) => {
+    console.log('Je fait : ' + type);
+}
+FaireQuelqueChose();
+
+FaireUneTache('la vaiselle');
+FaireUneTache('les courses');
